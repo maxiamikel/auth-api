a392304548d13d74fde9aea9c4395a0ea27c7e89
maxiamikel <maxloversist@gmail.com>
2024-02-11 22:14:00 -0300

    Add the spring security dependences to pom.xml, add the UserRole role to the User class and create the UserRole enum to generate the API securiry

---
+++ src/main/java/com/maxi/authapi/enums/UserRole.java
@@ -0,0 +1,20 @@
+package com.maxi.authapi.enums;
+
+public enum UserRole {
+    ADMIN("admin"),
+    USER("user");
+
+    private String role;
+
+    private UserRole(String role) {
+        this.role = role;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
--- src/main/java/com/maxi/authapi/models/User.java
+++ src/main/java/com/maxi/authapi/models/User.java
@@ -1,6 +1,17 @@
 package com.maxi.authapi.models;
 
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.maxi.authapi.enums.UserRole;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -12,22 +23,66 @@ import lombok.Setter;
 @Table(name = "td_users")
 @Getter
 @Setter
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String login;
-    private String password;
+    private String pwd;
+
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
 
     public User() {
     }
 
-    public User(String name, String login, String password) {
+    public User(String name, String login, String pwd) {
         this.name = name;
         this.login = login;
-        this.password = password;
+        this.pwd = pwd;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        if (this.role.equals(UserRole.ADMIN)) {
+            return List.of(
+                    new SimpleGrantedAuthority("ROLE_ADMIN"),
+                    new SimpleGrantedAuthority("ROLE_USER"));
+        }
+        return List.of(
+                new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return this.pwd;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.login;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 
 }
